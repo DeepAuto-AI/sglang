fbbd6cd5f0f75b58153a54b6cea028023543108a
AinL <gmlwns5176@gmail.com>
2025-02-05 14:26:58 +0000

    update BIG

--- python/sglang/srt/layers/attention/hip_radix_attention.py
+++ python/sglang/srt/layers/attention/hip_radix_attention.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import os
 
 """
 HiP Attention Backend for SGLang
@@ -39,6 +40,95 @@ class HiPRadixAttentionBackend(AttentionBackend):
         self.is_offload_enabled = model_runner.server_args.enable_hip_offload
 
         self.max_context_len = model_runner.model_config.context_len
+        
+        self.tp_rank = model_runner.tp_rank
+        
+        # NOTE: query caching: this is quite temporary one.
+        self.q_buffers = [
+            torch.zeros(
+                (
+                    1,
+                    self.hip_config.block_sparse_block_size_q,
+                    model_runner.model_config.num_attention_heads
+                    // model_runner.tp_size,
+                    model_runner.model_config.head_dim,
+                ),
+                device=torch.device(model_runner.device),
+                dtype=model_runner.dtype,
+            )
+            for _ in range(model_runner.model_config.num_hidden_layers)
+        ]
+        # NOTE: disable q caching
+        self.q_buffers = None
+
+        # NOTE: sliding window indices: this is quite temporary one.
+        diag_info_path = os.getenv("HIP_DIAG_INFO", None)
+        if diag_info_path is not None:
+            self.diag_sliding_window_indices = []
+            for layer_id in range(model_runner.model_config.num_hidden_layers):
+                require_dense = layer_id in self.hip_config.dense_layers
+                if len(self.hip_config.layers) == 2:
+                    layer_config = self.hip_config.layers[0 if require_dense else 1]
+                else:
+                    layer_config = self.hip_config.layers[layer_id]
+                exclude_window_size = layer_config.sliding_window_size // 2
+                diag_sliding_window_range = 131072
+                diag_sliding_window_size = 4096 if require_dense else 4096
+
+                chunk_size = layer_config.stages[-1].stage_chunk_size
+                block_size_q = layer_config.stages[-1].stage_block_size_q
+
+                diag_info = torch.load(
+                    diag_info_path, map_location=model_runner.device
+                )["prob_avg"][
+                    layer_id
+                ]  # type: torch.Tensor
+                assert diag_info.ndim == 2
+
+                diag_info = diag_info.sum(dim=0, keepdim=True).expand_as(diag_info)
+                diag_info[
+                    :, : max(0, diag_info.shape[-1] - diag_sliding_window_range)
+                ].fill_(-32000.0)
+                diag_info[:, -exclude_window_size:].fill_(-32000.0)
+                diag_info = torch.nn.functional.max_pool1d(
+                    diag_info.unsqueeze(1),
+                    kernel_size=max(chunk_size, block_size_q) + 1,
+                    stride=1,
+                    padding=max(chunk_size, block_size_q) // 2,
+                ).squeeze(1)
+                diag_info = diag_info[:, ::chunk_size]
+                _, indices = diag_info.topk(
+                    k=diag_sliding_window_size // chunk_size, dim=-1
+                )
+
+                head_per_tp = (
+                    model_runner.model_config.num_attention_heads
+                    // model_runner.tp_size
+                )
+                idx_head_start = head_per_tp * model_runner.tp_rank
+                idx_head_end = head_per_tp * (model_runner.tp_rank + 1)
+                self.diag_sliding_window_indices.append(
+                    (indices[idx_head_start:idx_head_end] - diag_info.shape[-1])
+                    * chunk_size
+                )
+        else:
+            self.diag_sliding_window_indices = None
+
+    def push_q_buffer(self, q: torch.Tensor, layer_id: int, batch_size: int):
+        if self.q_buffers is None:
+            return
+        assert batch_size == 1
+        q = q.unsqueeze(0)
+        layer_q_buffer = self.q_buffers[layer_id]
+        q_buffer = torch.cat([layer_q_buffer, q[:, -layer_q_buffer.shape[1] :]], dim=1)
+        layer_q_buffer.copy_(q_buffer[:, -layer_q_buffer.shape[1] :])
+
+    def get_q_buffer(self, layer_id: int, batch_size: int) -> torch.Tensor:
+        if self.q_buffers is not None:
+            assert batch_size == 1
+            return self.q_buffers[layer_id].flatten(0, 1)
+        else:
+            return None
 
     def init_forward_metadata(self, forward_batch: ForwardBatch):
         pass
@@ -112,6 +202,9 @@ class HiPRadixAttentionBackend(AttentionBackend):
             offload_cache = None
 
         q_reshaped = q.reshape(-1, layer.tp_q_head_num, layer.head_dim)
+        self.push_q_buffer(
+            q_reshaped, layer_id=layer.layer_id, batch_size=forward_batch.batch_size
+        )
 
         # Output tensor
         o = torch.empty_like(q_reshaped)
@@ -169,6 +262,9 @@ class HiPRadixAttentionBackend(AttentionBackend):
                         else None
                     ),
                     offloading_metadata=offloading_metadata,
+                    is_decode=False,
+                    diag_sliding_window_indices=self.diag_sliding_window_indices,
+                    layer_id=layer.layer_id,
                 )
 
                 o[start_len : start_len + seq_len] = o_req
@@ -228,6 +324,13 @@ class HiPRadixAttentionBackend(AttentionBackend):
             offload_cache, offloading_metadata = (
                 forward_batch.token_to_kv_pool.get_kv_buffer(layer.layer_id)
             )
+        
+        self.push_q_buffer(
+            q.contiguous().view(-1, layer.tp_q_head_num, layer.head_dim),
+            layer_id=layer.layer_id,
+            batch_size=forward_batch.batch_size,
+        )
+        q_for_masking = self.get_q_buffer(layer.layer_id, forward_batch.batch_size)
 
         o, metadata = self.forward_paged_hip(
             query=q.contiguous().view(-1, layer.tp_q_head_num, layer.head_dim),
@@ -254,6 +357,10 @@ class HiPRadixAttentionBackend(AttentionBackend):
                 if self.is_offload_enabled
                 else None
             ),
+            is_decode=True,
+            query_for_mask=q_for_masking,
+            diag_sliding_window_indices=self.diag_sliding_window_indices,
+            layer_id=layer.layer_id,
         )
 
         forward_batch.hip_metadata_cache_pool.set_hip_metadata_cache(
