6fcf36be2458c1560606e17e8cec7d58f57c4b6e
Geon Park <mujjingun@gmail.com>
2025-02-18 17:15:02 +0900

    call reset_decode_phase()

--- python/sglang/srt/model_executor/forward_batch_info.py
+++ python/sglang/srt/model_executor/forward_batch_info.py
@@ -290,6 +290,8 @@ class ForwardBatch:
         # Init HiP attention information
         if model_runner.hip_metadata_cache_pool is not None:
             ret.hip_metadata_cache_pool = model_runner.hip_metadata_cache_pool
+            if batch.hip_metadata_cached_stages < 0:
+                ret.hip_metadata_cache_pool.reset_decode_phase()
             ret.hip_metadata_cached_stages = batch.hip_metadata_cached_stages
 
         # Init lora information
