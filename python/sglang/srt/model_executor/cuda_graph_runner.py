b8612fe4d2da31df9bbc293658dea06ecf1efef9
Geon Park <mujjingun@gmail.com>
2025-02-20 15:59:40 +0900

    fix lint

--- python/sglang/srt/model_executor/cuda_graph_runner.py
+++ python/sglang/srt/model_executor/cuda_graph_runner.py
@@ -319,9 +319,7 @@ class CudaGraphRunner:
 
     def capture_configs(self):
         if self.enable_hip_attention:
-            from hip_attn.v1_2.paged_hip import (
-                cuda_graph_capture_configs,
-            )
+            from hip_attn.v1_2.paged_hip import cuda_graph_capture_configs
 
             return cuda_graph_capture_configs(self.hip_config)
         else:
