b44c565da2a737fe5e0f5ae17c0ebb63a2c5de5d
Geon Park <mujjingun@gmail.com>
2025-03-04 12:14:43 -0500

    Support HiP Attention
    Co-authored-by: Heejun Lee <gmlwns5176@gmail.com>
    Co-authored-by: Bumsik Kim <k.bumsik@gmail.com>

--- python/sglang/srt/configs/model_config.py
+++ python/sglang/srt/configs/model_config.py
@@ -60,6 +60,7 @@ class ModelConfig:
         enable_multimodal: Optional[bool] = None,
         dtype: str = "auto",
         quantization: Optional[str] = None,
+        is_context_extended: Optional[bool] = None,
         override_config_file: Optional[str] = None,
         is_draft_model: bool = False,
         hybrid_kvcache_ratio: Optional[float] = None,
@@ -182,7 +183,9 @@ class ModelConfig:
                     f"Warning: {reason} context_length ({context_length}) is greater than the derived context_length ({derived_context_len}). "
                     f"This may lead to incorrect model outputs or CUDA errors. Note that the derived context_length may differ from max_position_embeddings in the model's config."
                 )
-                if (
+                if is_context_extended:
+                    pass
+                elif (
                     get_bool_env_var("SGLANG_ALLOW_OVERWRITE_LONGER_CONTEXT_LEN")
                     or is_in_ci()  # FIXME: fix this special case
                 ):
@@ -192,8 +195,7 @@ class ModelConfig:
                     raise ValueError(
                         f"{msg} To allow overriding this maximum, set the env var SGLANG_ALLOW_OVERWRITE_LONGER_CONTEXT_LEN=1"
                     )
-            else:
-                self.context_len = context_length
+            self.context_len = context_length
         else:
             self.context_len = derived_context_len
 
@@ -323,6 +325,7 @@ class ModelConfig:
             quantization=server_args.quantization,
             hybrid_kvcache_ratio=server_args.hybrid_kvcache_ratio,
             model_impl=server_args.model_impl,
+            is_context_extended=server_args.enable_hip_attention,
             **kwargs,
         )
 
