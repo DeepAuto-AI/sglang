76a493c963902e78f3cccb32e5832ba4ef3f2c7a
Geon Park <mujjingun@gmail.com>
2025-03-18 17:21:30 -0400

    fix if statement

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -914,7 +914,6 @@ class ModelRunner:
 
     def init_attention_backend(self):
         """Init attention kernel backend."""
-
         if self.server_args.attention_backend == "flashinfer":
             if not self.use_mla_backend:
                 from sglang.srt.layers.attention.flashinfer_backend import (
@@ -971,7 +970,7 @@ class ModelRunner:
 
             self.attn_backend = FlashAttentionBackend(self)
 
-        if self.server_args.enable_hip_attention:
+        elif self.server_args.enable_hip_attention:
             from sglang.srt.layers.attention.hip_attention import HiPAttentionBackend
 
             self.attn_backend = HiPAttentionBackend(self)
