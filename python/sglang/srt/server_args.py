ffdfeed3b5714598b578f1ca9bb1b7acc9337f22
AinL <gmlwns5176@gmail.com>
2025-06-23 03:22:28 +0900

    fix

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -955,6 +955,7 @@ class ServerArgs:
                 "fa3",
                 "flashmla",
                 "cutlass_mla",
+                "hip_attention",
             ],
             default=ServerArgs.attention_backend,
             help="Choose the kernels for attention layers.",
@@ -1358,6 +1359,9 @@ class ServerArgs:
         args.pp_size = args.pipeline_parallel_size
         args.dp_size = args.data_parallel_size
         args.ep_size = args.expert_parallel_size
+        
+        if args.attention_backend == 'hip_attention':
+            args.enable_hip_attention = True
 
         if args.enable_hip_attention:
             from hip_attn.v1_2 import HiPAttentionConfig
